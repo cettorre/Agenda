c6315d972f358e54c7c89d845a52152f78634089
CE <cettorre@gmail.com>
2017-05-05 20:24:55 +0400

    working on html view with stringBUilder

--- vistaHtml_pack/vistaTablas.java
+++ vistaHtml_pack/vistaTablas.java
@@ -21,12 +21,14 @@ public static StringBuilder buildTablasHTML(Sala sala){
 	
 	
 	
-	
+	String[] horariosHtml ={"0-1","1-2","2-3","3-4","4-5","5-6","6-7","7-8","8-9","9-10","10-11","11-12","12-13","13-14","14-15","15-16","16-17","17-18","18-19","18-19","19-20","20-21","21-22","22-23","22-23","22-23","22-23","22-23"};	
+		
 	
 	tablaHorariosSemanales.append("<html>" +
 	           "<body>" +
 	           "<table>" +
 	           "<tr>" +
+	           "<th>HORA</th>" +
 	           "<th>" + sala.getDisponibilidades()[0].getFecha()+"</br>" +sala.getDisponibilidades()[0].getFecha().getDayOfWeek()+ "</th>" +
 	           "<th>" + sala.getDisponibilidades()[1].getFecha()+"</br>" +sala.getDisponibilidades()[1].getFecha().getDayOfWeek()+ "</th>" +
 	           "<th>" + sala.getDisponibilidades()[2].getFecha()+"</br>" +sala.getDisponibilidades()[2].getFecha().getDayOfWeek()+ "</th>" +
@@ -40,6 +42,8 @@ public static StringBuilder buildTablasHTML(Sala sala){
 	
 	for (int i = 0; i < 23; i++) {
 	    tablaHorariosSemanales.append("<tr><td>")
+	    .append(horariosHtml[i])
+	       .append("</td><td>")
 	       .append(sala.getDisponibilidades()[0].getSlotsHorasActividad()[i])
 	       .append("</td><td>")
 	       .append(sala.getDisponibilidades()[1].getSlotsHorasActividad()[i])
@@ -66,6 +70,7 @@ public static StringBuilder buildTablasHTML(Sala sala){
 	           "<body>" +
 	           "<table>" +
 	           "<tr>" +
+	           "<th>HORA</th>" +
 	           "<th>" + sala.getDisponibilidades()[7].getFecha()+"</br>" +sala.getDisponibilidades()[7].getFecha().getDayOfWeek()+ "</th>" +
 	           "<th>" + sala.getDisponibilidades()[8].getFecha() +"</br>" +sala.getDisponibilidades()[8].getFecha().getDayOfWeek()+ "</th>" +
 	           "<th>" + sala.getDisponibilidades()[9].getFecha() +"</br>" +sala.getDisponibilidades()[9].getFecha().getDayOfWeek()+ "</th>" +
@@ -79,6 +84,8 @@ public static StringBuilder buildTablasHTML(Sala sala){
 	
 	for (int i = 0; i < 23; i++) {
 	    tablaHorariosSemanales.append("<tr><td>")
+	    .append(horariosHtml[i])
+	       .append("</td><td>")
 	       .append(sala.getDisponibilidades()[7].getSlotsHorasActividad()[i])
 	       .append("</td><td>")
 	       .append(sala.getDisponibilidades()[8].getSlotsHorasActividad()[i])
@@ -104,6 +111,7 @@ public static StringBuilder buildTablasHTML(Sala sala){
 	           "<body>" +
 	           "<table>" +
 	           "<tr>" +
+	           "<th>HORA</th>" +
 	           "<th>" + sala.getDisponibilidades()[14].getFecha() +"</br>" +sala.getDisponibilidades()[14].getFecha().getDayOfWeek()+ "</th>" +
 	           "<th>" + sala.getDisponibilidades()[15].getFecha() +"</br>" +sala.getDisponibilidades()[15].getFecha().getDayOfWeek()+ "</th>" +
 	           "<th>" + sala.getDisponibilidades()[16].getFecha() +"</br>" +sala.getDisponibilidades()[16].getFecha().getDayOfWeek()+ "</th>" +
@@ -117,6 +125,8 @@ public static StringBuilder buildTablasHTML(Sala sala){
 	
 	for (int i = 0; i < 23; i++) {
 	    tablaHorariosSemanales.append("<tr><td>")
+	    .append(horariosHtml[i])
+	       .append("</td><td>")
 	       .append(sala.getDisponibilidades()[14].getSlotsHorasActividad()[i])
 	       .append("</td><td>")
 	       .append(sala.getDisponibilidades()[15].getSlotsHorasActividad()[i])
@@ -141,6 +151,7 @@ public static StringBuilder buildTablasHTML(Sala sala){
 	           "<body>" +
 	           "<table>" +
 	           "<tr>" +
+	           "<th>HORA</th>" +
 	           "<th>" + sala.getDisponibilidades()[21].getFecha() +"</br>" +sala.getDisponibilidades()[21].getFecha().getDayOfWeek()+ "</th>" +
 	           "<th>" + sala.getDisponibilidades()[22].getFecha() +"</br>" +sala.getDisponibilidades()[22].getFecha().getDayOfWeek()+ "</th>" +
 	           "<th>" + sala.getDisponibilidades()[23].getFecha() +"</br>" +sala.getDisponibilidades()[23].getFecha().getDayOfWeek()+ "</th>" +
@@ -154,6 +165,8 @@ public static StringBuilder buildTablasHTML(Sala sala){
 	
 	for (int i = 0; i < 23; i++) {
 	    tablaHorariosSemanales.append("<tr><td>")
+	    .append(horariosHtml[i])
+	       .append("</td><td>")
 	       .append(sala.getDisponibilidades()[21].getSlotsHorasActividad()[i])
 	       .append("</td><td>")
 	       .append(sala.getDisponibilidades()[22].getSlotsHorasActividad()[i])
@@ -180,32 +193,35 @@ public static StringBuilder buildTablasHTML(Sala sala){
 	           "<body>" +
 	           "<table>" +
 	           "<tr>" +
+	           "<th>HORA</th>" +
 	           "<th>" + sala.getDisponibilidades()[28].getFecha() +"</br>" +sala.getDisponibilidades()[28].getFecha().getDayOfWeek()+ "</th>" +
 	           "<th>" + sala.getDisponibilidades()[29].getFecha() +"</br>" +sala.getDisponibilidades()[29].getFecha().getDayOfWeek()+ "</th>" +
-	           "<th>" + sala.getDisponibilidades()[27].getFecha() +"</br>" +sala.getDisponibilidades()[27].getFecha().getDayOfWeek()+ "</th>" +
-	           "<th>" + sala.getDisponibilidades()[0].getFecha() +"</br>" +sala.getDisponibilidades()[0].getFecha().getDayOfWeek()+ "</th>" +
-	           "<th>" + sala.getDisponibilidades()[0].getFecha() +"</br>" +sala.getDisponibilidades()[0].getFecha().getDayOfWeek()+ "</th>" +
-	           "<th>" + sala.getDisponibilidades()[0].getFecha() +"</br>" +sala.getDisponibilidades()[0].getFecha().getDayOfWeek()+ "</th>" +
-	           "<th>" + sala.getDisponibilidades()[0].getFecha() +"</br>" +sala.getDisponibilidades()[0].getFecha().getDayOfWeek()+ "</th>" +
+	           "<th>" + sala.getDisponibilidades()[0].getFecha() +"</br>" +sala.getDisponibilidades()[27].getFecha().getDayOfWeek()+ "</th>" +
+	           "<th>" + sala.getDisponibilidades()[1].getFecha() +"</br>" +sala.getDisponibilidades()[0].getFecha().getDayOfWeek()+ "</th>" +
+	           "<th>" + sala.getDisponibilidades()[2].getFecha() +"</br>" +sala.getDisponibilidades()[0].getFecha().getDayOfWeek()+ "</th>" +
+	           "<th>" + sala.getDisponibilidades()[3].getFecha() +"</br>" +sala.getDisponibilidades()[0].getFecha().getDayOfWeek()+ "</th>" +
+	           "<th>" + sala.getDisponibilidades()[4].getFecha() +"</br>" +sala.getDisponibilidades()[0].getFecha().getDayOfWeek()+ "</th>" +
 	           "</tr>");
 	
 	
 	
 	for (int i = 0; i < 23; i++) {
 	    tablaHorariosSemanales.append("<tr><td>")
+	    .append(horariosHtml[i])
+	       .append("</td><td>")
 	       .append(sala.getDisponibilidades()[28].getSlotsHorasActividad()[i])
 	       .append("</td><td>")
 	       .append(sala.getDisponibilidades()[29].getSlotsHorasActividad()[i])
 	       .append("</td><td>")
-	       .append(sala.getDisponibilidades()[27].getSlotsHorasActividad()[i])
-	       .append("</td><td>")
 	       .append(sala.getDisponibilidades()[0].getSlotsHorasActividad()[i])
 	       .append("</td><td>")
-	       .append(sala.getDisponibilidades()[0].getSlotsHorasActividad()[i])
+	       .append(sala.getDisponibilidades()[1].getSlotsHorasActividad()[i])
 	       .append("</td><td>")
-	       .append(sala.getDisponibilidades()[0].getSlotsHorasActividad()[i])
+	       .append(sala.getDisponibilidades()[2].getSlotsHorasActividad()[i])
 	       .append("</td><td>")
-	       .append(sala.getDisponibilidades()[0].getSlotsHorasActividad()[i])
+	       .append(sala.getDisponibilidades()[3].getSlotsHorasActividad()[i])
+	       .append("</td><td>")
+	       .append(sala.getDisponibilidades()[4].getSlotsHorasActividad()[i])
 	       .append("</td></tr>");
 	}
 	tablaHorariosSemanales.append("</table>" +
